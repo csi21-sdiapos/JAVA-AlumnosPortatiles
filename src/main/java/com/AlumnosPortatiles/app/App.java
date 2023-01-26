ce84e4dc284c2bc4ea5170571f465cb53fffaccc
csi21-sdiapos <csi21-sdiapos@colegioaltair.net>
2023-01-26 14:08:40 +0100

    entities created

---
+++ src/main/java/com/AlumnosPortatiles/app/App.java
@@ -0,0 +1,13 @@
+package com.AlumnosPortatiles.app;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("App")
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
